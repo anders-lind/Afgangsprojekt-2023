5bf794528753f9cd264d56173f6d8fe19bcc7c06
Peter Skibdal Frydensberg <frydensberg.peter@gmail.com>
2023-11-22 15:40:42 +0100

    Added changes to human_cost with regards to indexing and figures

--- local_planners/human_cost.py
+++ local_planners/human_cost.py
@@ -5,7 +5,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from math import *
 
 class Human_cost:
-    def __init__(self, mu_x = 0, var_x = 3, mu_y = 0, var_y = 12):
+    def __init__(self, mu_x = 0, var_x = 12, mu_y = 0, var_y = 3):
         self.mu_x = mu_x
         self.var_x = var_x
         self.mu_y = mu_y
@@ -18,27 +18,33 @@ class Human_cost:
         self.personal = 4*0.3 + self.person_size
         self.social = 10*0.3 + self.person_size
         
+        self.nx = 500
+        self.ny = 500
         
-        self.x = np.linspace(-7,7,500)
-        self.y = np.linspace(-7,7,500)
+        x_min = -10
+        x_max = 10
+        
+        y_min = -10
+        y_max = 10
+        
+        self.x = np.linspace(x_min,x_max,self.nx)
+        self.y = np.linspace(y_min,y_max,self.ny)
         
         self.create_graph(visualize=False)
     
     def create_graph(self, visualize = True):
         #Create grid and multivariate normal
-        self.x  = np.linspace(-7,7,500)
-        self.y  = np.linspace(-7,7,500)
-        Y, X = np.meshgrid(self.x , self.y)
-        pos = np.empty(X.shape + (2,))
-        pos[:, :, 0] = X; pos[:, :, 1] = Y
-        rv = multivariate_normal([self.mu_x, self.mu_y], [[self.var_x, 0], [0, self.var_y]])
-
-        pdf = rv.pdf(pos)
-        skew = rv.pdf(pos)
-        skewpdf = rv.pdf(pos)
-        scaling = rv.pdf(pos)
         
-        self.cost_func = rv.pdf(pos)
+        X, Y = np.meshgrid(self.x , self.y, indexing="ij")
+        pos = np.dstack((X, Y))
+        rv = multivariate_normal([self.mu_x, self.mu_y], [[self.var_x, 0], [0, self.var_y]])
+        
+        pdf = rv.pdf(pos)
+
+        skew = np.empty((self.nx, self.ny))
+        skewpdf = skew.copy()
+        scaling = skew.copy()
+        self.cost_func = skew.copy()
 
         b = 1
         c = 3
@@ -46,39 +52,37 @@ class Human_cost:
 
         max = pdf[250,250]
 
+        for i in range(self.nx):
+            for j in range(self.ny):
 
-
-        for i in range(np.shape(self.x)[0]):
-            for j in range(np.shape(self.y)[0]):
-                # Unit
-                pdf[j,i] = pdf[j,i] * 1/max
+                pdf[i,j] = pdf[i,j] * 1/max
 
                 # only asym
-                if self.y[j] > 0:
-                    skew[j,i] = -a*self.y[j]**2+b
-                if self.y[j] > c:
-                    skew[j,i] = 0
-                elif self.y[j] <= 0:
-                    skew[j,i] = 1
+                if self.x[i] < 0:
+                    skew[i,j] = -a*self.x[i]**2+b
+                if self.x[i] < -c:
+                    skew[i,j] = 0
+                elif self.x[i] > 0:
+                    skew[i,j] = 1
                 
 
                 # Only scale
                 if dist((self.x[i],self.y[j]), (0,0)) < self.person_size:
-                    scaling[j,i] = 1.1
+                    scaling[i,j] = 1.1
                 elif dist((self.x[i],self.y[j]), (0,0)) < self.intimate:
-                    scaling[j,i] = 1.0
+                    scaling[i,j] = 1.0
                 elif dist((self.x[i],self.y[j]), (0,0)) < self.personal:
-                    scaling[j,i] = 0.5
+                    scaling[i,j] = 0.5
                 elif dist((self.x[i],self.y[j]), (0,0)) < self.social:
-                    scaling[j,i] = 0.25
+                    scaling[i,j] = 0.25
                 elif dist((self.x[i],self.y[j]), (0,0)) >= self.social:
-                    scaling[j,i] = 0.0
+                    scaling[i,j] = 0.0
                 
                 # skew pdf
-                skewpdf[j,i] = pdf[j,i] * skew[j,i]
+                skewpdf[i,j] = pdf[i,j] * skew[i,j]
 
                 # Asym skew
-                self.cost_func[j,i] = pdf[j,i] * skew[j,i] * scaling[j,i]
+                self.cost_func[i,j] = pdf[i,j] * skew[i,j] * scaling[i,j]
                 
         if visualize == True:
             #Make a 3D plot
@@ -89,32 +93,32 @@ class Human_cost:
             ax1.plot_surface(X, Y, self.cost_func,cmap='viridis',linewidth=0)
             ax1.set_xlabel('X axis')
             ax1.set_ylabel('Y axis')
-            ax1.set_zlabel('Z axis')
+            ax1.set_zlabel('Cost C(x,y)')
 
             ax2 = plt.figure("skew").add_subplot(projection='3d')
             ax2.plot_surface(X, Y, skew,cmap='viridis',linewidth=0)
             ax2.set_xlabel('X axis')
             ax2.set_ylabel('Y axis')
-            ax2.set_zlabel('Z axis')
+            ax2.set_zlabel('Cost C(x,y)')
 
             ax3 = plt.figure("Scaling").add_subplot(projection='3d')
             ax3.plot_surface(X, Y, scaling,cmap='viridis',linewidth=0)
             ax3.set_xlabel('X axis')
             ax3.set_ylabel('Y axis')
-            ax3.set_zlabel('Z axis')
+            ax3.set_zlabel('Cost C(x,y)')
 
             ax4 = plt.figure("pdf").add_subplot(projection='3d')
             plt.title("pdf")
             ax4.plot_surface(X, Y, pdf,cmap='viridis',linewidth=0)
             ax4.set_xlabel('X axis')
             ax4.set_ylabel('Y axis')
-            ax4.set_zlabel('Z axis')
+            ax4.set_zlabel('Cost C(x,y)')
 
             ax5 = plt.figure("skew pdf").add_subplot(projection='3d')
             ax5.plot_surface(X, Y, skewpdf,cmap='viridis',linewidth=0)
             ax5.set_xlabel('X axis')
             ax5.set_ylabel('Y axis')
-            ax5.set_zlabel('Z axis')
+            ax5.set_zlabel('Cost C(x,y)')
 
             plt.show()
                     
@@ -143,7 +147,7 @@ class Human_cost:
         
         
 if __name__ == "__main__":
-    cost = Human_cost(mu_x = 0, var_x = 3, mu_y = 0, var_y = 12)
+    cost = Human_cost()
     
     x = 1  #m
     y = 1  #m
